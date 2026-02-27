218c61541152aae78c5bfde092cec46cf8becd34
Nivi13Git <nivedithamariraj13@gmail.com>
2026-02-27 18:16:32 +0530

    Add/update chapter_02 files

---
+++ chapter_02_Java_Concepts/07_let_var_const.js
@@ -0,0 +1,30 @@
+/* function printHello(){
+        console.log("Hello TheTestingAcademy!");
+        let b = 30; // Local Scope
+        console.log(b);
+        if(true){
+            let b = 5;
+            console.log(b); // 5
+        }
+        console.log("let ->",b);   
+}
+
+// Calling of the function
+printHello(); */
+
+function printHello()
+{
+
+console.log("Hello TheTestingAcademy!");
+  var a = 20; // Local Scope
+console.log(a);
+if(true){
+var a = 30;
+console.log(a); // 30  
+ }
+ console.log("F ->",a);
+}
+
+
+// // Calling of the function 
+printHello();
